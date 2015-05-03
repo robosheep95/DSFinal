38ce23f6030a06a418602674cb11369d507d30ab
robosheep95 <scafetaylor@gmail.com>
2015-05-03 10:52:35 -0500

    Initial Import
    Moved workspace to GitHub. Has basic experimental functions.

---
+++ myTable.py
@@ -0,0 +1,33 @@
+01 02 03 04 05 06 07 08 09
+10 11 12 13 14 15 16 17 18
+19 20 21 22 23 24 25 26 27
+28 29 30 31 32 33 34 35 36
+37 38 39 40 41 42 43 44 45
+46 47 48 49 50 51 52 53 54
+55 56 57 58 59 60 61 62 63
+64 65 66 67 68 69 70 71 72
+73 74 75 76 77 78 79 80 81
+
+
+#To find certian x and y
+
+def getSpace(x,y,table):
+    #returns List at x and y of list table
+    space = table[(x+(9*(y-1)))]
+    return space
+
+def check(self,number):
+    if number in line():
+        return False
+    if number in coloum():
+        return False
+    return True
+
+def line(self,table):
+    y = table.getLine
+    for i in range((9*(y-1)+1),(9*(y-1)+9)+1)
+
+
+def coloum():
+
+
