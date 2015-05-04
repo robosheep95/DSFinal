9cee7ad84acceab13eaa898fe82caced64b18a58
Taylor Scafe <scafetaylor@gmail.com>
2015-05-04 10:39:34 -0500

    2nd commit on Main
    Added the start of the algorithm

---
+++ mainSudoku.py
@@ -0,0 +1,15 @@
+import * in myTable()
+def solve():
+	solved = False
+	table = myTable()
+	table.fill()
+	while table.single() == False:
+		pass
+	if table.isSolved == False:
+	## Do stuff
+	
+	
+
+
+
+def create()
