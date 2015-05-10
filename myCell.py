deff403fd1e5c1f701586b32beb46ae7dbbc2ca7
JHuddle8 <jlh1@sting.graceland.edu>
2015-05-10 13:27:20 -0500

    worked on myCell Class
    few fixes and changes

--- myCell.py
+++ myCell.py
@@ -1,47 +1,45 @@
 class myCell():
-	def __init__(self):
-		self.pos = []
-		self.col = 0
-		self.row = 0
-		self.answer = 0
-	
-	def setRow(self, x):
-		self.row = x
-	
-	def setCol(self, y):
-		self.col = y
-		
-	def getRow(self):
-		return self.row
-	
-	def getCol(self):
-		return self.col
-		
-	def getBox(self, box):
-		self.box = box
-	
-	def setBox(self):
-		return self.box
-	
-	def setAnswer(self,value):
-		self.answer = value
-		
-	def getAnswer(self):
-		return self.answer
-		
-	def getPos(self):
-		return self.pos
-	
-	def __add__(self, inp):
-		if inp != in self.pos:
-			self.pos.append(inp)
-		else:
-			pass
-	
-	def __sub__(self, inp):
-		if inp == in self.pos:
-			self.pos.remove(inp)
-		else:
-			pass
-		
-		
+    def __init__(self):
+        self.pos = []
+        self.col = 0
+        self.row = 0
+        self.answer = 0
+
+    def setRow(self, x):
+        self.row = x
+    
+    def setCol(self, y):
+        self.col = y
+            
+    def getRow(self):
+        return self.row
+    
+    def getCol(self):
+        return self.col
+            
+    def getBox(self, box):
+        self.box = box
+    
+    def setBox(self):
+        return self.box
+    
+    def setAnswer(self,value):
+        self.answer = value
+            
+    def getAnswer(self):
+        return self.answer
+            
+    def getPos(self):
+        return self.pos
+    
+    def add(self, inp):
+        if inp not in self.pos:
+            self.pos.append(inp)
+        else:
+            pass
+            
+    def __sub__(self, inp):
+        if inp in self.pos:
+            self.pos.remove(inp)
+        else:
+            pass
