da70921d3a7363a64fea3f1a0695a54281a96144
JHuddle8 <jlh1@sting.graceland.edu>
2015-05-11 09:39:03 -0500

    may 11 2015
    Add doc strings, clean up code, input test puzzles

--- myTable.py
+++ myTable.py
@@ -1,4 +1,9 @@
-from myCell import *
+"""
+Sudoku Solver
+Taylor Scafe, Jordan Huddleston
+May 11, 2015
+"""
+
 ##01 02 03 04 05 06 07 08 09
 ##10 11 12 13 14 15 16 17 18
 ##19 20 21 22 23 24 25 26 27
@@ -9,15 +14,12 @@ from myCell import *
 ##64 65 66 67 68 69 70 71 72
 ##73 74 75 76 77 78 79 80 81
 
+from myCell import *
 
-
-#To find certian x and y
 class myTable():
 
-
-
-        
     def __init__(self,newdata):
+        """Create table with row, column, and box. Takes input and fills in answers."""
         self.board = []
         for i in range(1,82):
             cell = myCell()
@@ -31,35 +33,31 @@ class myTable():
         for cell in self.board:
             cell.setAnswer(int(newdata[count]))
             count +=1
-
-
-##    def getPos(self,x,y):
-##        #returns List at x and y of list table
-##        cell =getPos(table[(x+(9*(y-1)))])
-##        return cell
         
     def fill(self,board):
+        """Takes input value in range, places in table"""
         for cell in board:
             if cell.getAnswer() == 0:
                 for value in range(1,10):
                     if self.check(cell.getRow(),cell.getCol(),cell.getBox(),value,board):
                         cell.addPos(value)
+                        
     def check(self,row,col,box,value,board):
+        """Used by fill, checks if value is a valid possibility."""
         for cell in board:
-            #print("{},{},{},{}".format(checkCell.getRow(),checkCell.getCol(),checkCell.getBox(),value))
             #print("{},{},{}".format(cell.getRow(),cell.getCol(),cell.getBox()))
             if row == cell.getRow() and value == cell.getAnswer():
                 return False
             elif col == cell.getCol() and value == cell.getAnswer():
                 return False
             elif box == cell.getBox() and value == cell.getAnswer():
-                return False
-                        
+                return False          
         else:
             return True
 
         
     def simple(self,board):
+        """Checks all cells for single possibilities, then calls removeCheck."""
         done = False
         while not done:
             done = True
@@ -74,6 +72,7 @@ class myTable():
                 
         
     def removeCheck(self,row,col,box,value,board):
+        """Removes possibilities in other cells after new possibility inked in"""
         for cell in board:
             if row == cell.getRow() and value in cell.getPos():
                 cell.removePos(value)
@@ -88,32 +87,22 @@ class myTable():
 
     
     def guess(self,board):
+        """Breaks cells that have two possibilites into two seperate tables. Then solves."""
         self.sortMinPos(self.board)
         output1 = board[:]
         output2 = board[:]
-        
         for cell in output1:
             if cell.getLen()>0:
                 print(cell.getPos())
-                cell.setPos(cell.getPos()[0])
-                
+                cell.setPos(cell.getPos()[0])   
         for cell in output2:
             if cell.getLen()>0:
                 print(cell.getPos())
                 cell.setPos(cell.getPos()[1])
-             
         return(output1,output2)
         
-               ## cell.setPos(newpos[0])
-                    
-                
-##                self.removeCheck(cell.getRow(),cell.getCol(),cell.getBox(),cell.getAnswer(),board)
-##                self.stack.push(newboard)
-##                break
-##                self.solve(self.stack.pop())
-                
-
     def isBad(self,board):
+        """If cell in table returns no answer and no possibilities, returns True."""
         for cell in board:
             if cell.getAnswer() == 0 and cell.getLen() == 0:
                 return True
@@ -121,95 +110,19 @@ class myTable():
 
 
     def isFinished(self,board):
+        """If every cell has valid answer, returns True"""
         for cell in board:
             if cell.getAnswer() == 0:
                 return False
         return True
 
 ####main
-t = myTable()
-##t.board[0].setAnswer(6)
-##t.board[1].setAnswer(0)
-##t.board[2].setAnswer(7)
-##t.board[3].setAnswer(0)
-##t.board[4].setAnswer(3)
-##t.board[5].setAnswer(8)
-##t.board[6].setAnswer(0)
-##t.board[7].setAnswer(5)
-##t.board[8].setAnswer(0)
-##t.board[9].setAnswer(0)
-##t.board[10].setAnswer(0)
-##t.board[11].setAnswer(0)
-##t.board[12].setAnswer(0)
-##t.board[13].setAnswer(2)
-##t.board[14].setAnswer(1)
-##t.board[15].setAnswer(0)
-##t.board[16].setAnswer(0)
-##t.board[17].setAnswer(4)
-##t.board[18].setAnswer(4)
-##t.board[19].setAnswer(0)
-##t.board[20].setAnswer(0)
-##t.board[21].setAnswer(0)
-##t.board[22].setAnswer(0)
-##t.board[23].setAnswer(6)
-##t.board[24].setAnswer(7)
-##t.board[25].setAnswer(0)
-##t.board[26].setAnswer(0)
-##t.board[27].setAnswer(1)
-##t.board[28].setAnswer(0)
-##t.board[29].setAnswer(0)
-##t.board[30].setAnswer(0)
-##t.board[31].setAnswer(0)
-##t.board[32].setAnswer(0)
-##t.board[33].setAnswer(8)
-##t.board[34].setAnswer(9)
-##t.board[35].setAnswer(0)
-##t.board[36].setAnswer(0)
-##t.board[37].setAnswer(0)
-##t.board[38].setAnswer(0)
-##t.board[39].setAnswer(0)
-##t.board[40].setAnswer(0)
-##t.board[41].setAnswer(0)
-##t.board[42].setAnswer(0)
-##t.board[43].setAnswer(0)
-##t.board[44].setAnswer(0)
-##t.board[45].setAnswer(0)
-##t.board[46].setAnswer(7)
-##t.board[47].setAnswer(8)
-##t.board[48].setAnswer(0)
-##t.board[49].setAnswer(0)
-##t.board[50].setAnswer(0)
-##t.board[51].setAnswer(0)
-##t.board[52].setAnswer(0)
-##t.board[53].setAnswer(5)
-##t.board[54].setAnswer(0)
-##t.board[55].setAnswer(0)
-##t.board[56].setAnswer(4)
-##t.board[57].setAnswer(8)
-##t.board[58].setAnswer(0)
-##t.board[59].setAnswer(0)
-##t.board[60].setAnswer(0)
-##t.board[61].setAnswer(0)
-##t.board[62].setAnswer(7)
-##t.board[63].setAnswer(8)
-##t.board[64].setAnswer(0)
-##t.board[65].setAnswer(0)
-##t.board[66].setAnswer(2)
-##t.board[67].setAnswer(9)
-##t.board[68].setAnswer(0)
-##t.board[69].setAnswer(0)
-##t.board[70].setAnswer(0)
-##t.board[71].setAnswer(0)
-##t.board[72].setAnswer(0)
-##t.board[73].setAnswer(5)
-##t.board[74].setAnswer(0)
-##t.board[75].setAnswer(6)
-##t.board[76].setAnswer(4)
-##t.board[77].setAnswer(0)
-##t.board[78].setAnswer(1)
-##t.board[79].setAnswer(0)
-##t.board[80].setAnswer(2)
-##
+    
+string1 = "0 0 2 1 8 0 0 6 5 0 7 0 9 0 0 8 0 0 6 3 0 0 0 2 0 0 1 5 0 0 6 2 0 7 0 0 7 9 0 0 0 0 0 4 2 0 0 4 0 1 9 0 0 8 8 0 0 2 0 0 0 9 3 0 0 6 0 0 5 0 8 0 4 2 0 0 7 3 1 0 0"
+string2 = "0 2 0 0 9 0 0 6 0 9 0 3 0 0 0 4 0 1 0 0 1 4 0 3 7 0 0 3 0 0 7 0 6 0 0 5 0 0 0 2 0 9 0 0 0 8 0 0 5 0 1 0 0 2 0 0 9 8 0 4 1 0 0 6 0 5 0 0 0 9 0 8 0 4 0 0 1 0 0 3 0"
+string3 = "0 0 0 8 0 0 0 0 2 5 0 0 7 0 0 8 0 9 0 0 2 0 0 0 0 0 6 1 0 0 0 5 0 2 9 0 0 0 0 4 0 8 0 0 0 0 4 3 0 9 0 0 0 1 3 0 0 0 0 0 9 0 0 9 0 6 0 0 1 0 0 8 8 0 0 0 0 6 0 0 0"
+
+t = myTable(string3)
 t.fill(t.board)
 for cell in t.board:
     print(cell.getPos())
